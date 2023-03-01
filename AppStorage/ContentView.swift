13151ac986aa4c8c0f07b08d1d23f0f3d09b4a5c
Abishan <highoncode09@gmail.com>
2023-03-01 08:40:32 +0530

    App Data Storage

--- AppStorage/ContentView.swift
+++ AppStorage/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var name: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            TextField("Enter your name", text: $name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .cornerRadius(30)
+            
+            Button {
+                //
+            } label: {
+                Text("Submit")
+            }
+
+            
+            Text("Your name is \(name)")
         }
         .padding()
     }
